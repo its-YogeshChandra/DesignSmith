a29bfbda4ef27919dc188a6beb4e5138802e4051
its-YogeshChandra <pandittheroyal@gmail.com>
2026-09-07 20:04:07 +0530

    initializing DesignSmith

---
+++ src/designsmith/ utils/file_utility.py
@@ -0,0 +1,5 @@
+#the goal is to read the image files from the folder or from the cloudinary api
+import requests; 
+import boto3;
+
+#import from the cloudinary or cloudflare 2 bucket  
---
+++ src/designsmith/__init__.py
@@ -0,0 +1,8 @@
+import llama_cloud;
+
+
+
+
+
+def main() -> None:
+    print("Hello from designsmith!")
