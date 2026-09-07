8febfb2d1a367d8500eb15abb7e8d6c94590bcb5
its-YogeshChandra <pandittheroyal@gmail.com>
2026-09-07 23:21:48 +0530

    refactor: relocate file utility module and integrate S3 storage access into the indexing pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,5 +1,13 @@
 #goal : index all the images using llamaindex and save it to qdrant 
-import llama_cloud;
+from llama_cloud import LlamaCloud;
+from designsmith.utils.file_utility import download_files_from_s3, list_objects;
 
 #need to configure the llama cloud first
 #using async configurations 
+file_vector = list_objects();
+client = LlamaCloud()
+
+#function is the main indexing function
+def indexer():
+    file = client.files.create(file="", purpose="")
+    result = client.parsing.parse(file_id)
